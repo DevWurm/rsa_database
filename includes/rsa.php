a467d02172916d05549621f154bdb3f3f1e4c2fc
DevWurm <devwurm@gmx.net>
2015-05-06 17:04:02 +0200

    Adding ascii to int converter, moving source  file into correct directory

---
+++ includes/rsa.php
@@ -0,0 +1,21 @@
+<?
+
+function convert_to_int($ascii_value) {
+	$int_representation = array();
+	for ($i = 0; i <= strlen($ascii_value)-1; $i++) {
+		array_push($int_representation, ord(substr($ascii_value, $i, 1))); //convert stricg character at i to ASCII int representation and add to $int_representation
+	}
+	return $int_representation;
+}
+
+
+function codieren ($array) {
+	foreach ($array as $k) {
+		
+	}
+		
+	return $codiert;
+}
+
+
+?>
