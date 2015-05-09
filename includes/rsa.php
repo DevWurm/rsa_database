b10d038f6f1743263acd0186e10680dc8d1d5cb7
Enceladus-2-89 <enceladus.2.89@gmail.com>
2015-05-09 13:51:25 +0200

    adjusting the function encrypt and decrypt to the former changes

--- includes/rsa.php
+++ includes/rsa.php
@@ -1,15 +1,15 @@
 <?
 include_once("converter.php");
 
-function encode ($array, $e, $n) { //rsa encode array with public keys e and N; decode must be include separetly 
+function encode ($array, $pubkeye, $pubkeyN) { //rsa encode array with public keys e and N; decode must be include separetly 
 	$encoded = array();
 	foreach ($array as $k) {
-		$k = mod($k, $e, $n);
+		$k = mod($k, $pubkeye, $pubkeyN);
 	}
 	return $encoded;
 }
 
-function decode ($array, $d, $n) { //rsa decode array with privat key d and public key N
+function decode ($array, $privd, $pubkeyN) { //rsa decode array with privat key d and public key N
 	
 }
 
@@ -17,15 +17,15 @@ function mod ($basis, $exponent, $n) { //returns the result of basis^exponent mo
 	
 }
 
-function encrypt ($string, $pubkey) { // rsa encrypt string with pubkey and return database string
+function encrypt ($string, $pubkeye, $pubkeyN) { // rsa encrypt string with public key e and N and return database string
 	$int_representation = convert_ascii_to_int($string); //convert string to int array
-	$encrypted_array = encode($int_representation, $pubkey); //encrypt int array
+	$encrypted_array = encode($int_representation, $pubkeye, $pubkeyN); //encrypt int array
 	return convert_int_array_to_db_string($encrypted_array); //convert array into seperator based string
 }
 
-function decrypt ($db_string, $privkey) {// rsa decrypt databse string with privkey and return string
+function decrypt ($db_string, $privkeyd, $pubkeyN) {// rsa decrypt databse string with privkey d and public key N and return string
 	$encrypted_array = convert_db_string_to_int_array($db_string); //convert seperator based string to int array 
-	$int_representation = encode($encrypted_array, $privkey); //decrypt int array
+	$int_representation = decode($encrypted_array, $privkeyd, $pubkeyN); //decrypt int array
 	return convert_int_to_ascii($int_representation); //convert int array into string
 }
 
