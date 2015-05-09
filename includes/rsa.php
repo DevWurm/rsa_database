7bfe4ad4c7525f1a93040c2ab57b07c6fb5d26cb
DevWurm <devwurm@gmx.net>
2015-05-09 14:58:32 +0200

    Merging encode and decode function

--- includes/rsa.php
+++ includes/rsa.php
@@ -1,25 +1,19 @@
 <?
 include_once("converter.php");
 
-function encode ($array, $pubkeye, $pubkeyN) { //rsa encode array with public keys e and N; decode must be include separetly 
-	$encoded = array();
-	foreach ($array as $k) {
-		$k = mod($k, $pubkeye, $pubkeyN);
-	}
-	return $encoded;
-}
-
-function decode ($array, $privkeyd, $pubkeyN) { //rsa decode array with privat key d and public key N
-	$decoded = array();
-	foreach ($array as $k) {
-		$k = mod($k, $privkeyd, $pubkeyN);
-	}
-	return $decoded;
-}
 
 function mod ($basis, $exponent, $n) { //returns the result of basis^exponent modulo n
 	
 }
+
+function encode ($array, $key, $keyN) { //rsa encode array with public keys e and N; decode must be include separetly 
+	$encoded = array();
+	foreach ($array as $k) {
+		$k = mod($k, $key, $keyN);
+	}
+	return $encoded;
+}
+
 
 function encrypt ($string, $pubkeye, $pubkeyN) { // rsa encrypt string with public key e and N and return database string
 	$int_representation = convert_ascii_to_int($string); //convert string to int array
@@ -29,7 +23,7 @@ function encrypt ($string, $pubkeye, $pubkeyN) { // rsa encrypt string with publ
 
 function decrypt ($db_string, $privkeyd, $pubkeyN) {// rsa decrypt databse string with privkey d and public key N and return string
 	$encrypted_array = convert_db_string_to_int_array($db_string); //convert seperator based string to int array 
-	$int_representation = decode($encrypted_array, $privkeyd, $pubkeyN); //decrypt int array
+	$int_representation = encode($encrypted_array, $privkeyd, $pubkeyN); //decrypt int array
 	return convert_int_to_ascii($int_representation); //convert int array into string
 }
 
