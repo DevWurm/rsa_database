bb2dc8add4672c010bfd34e1475c93a45a9ea5f2
Enceladus-2-89 <enceladus.2.89@gmail.com>
2015-05-09 13:45:31 +0200

    adding the encode function and creating the function decode in order to implement it later

--- includes/rsa.php
+++ includes/rsa.php
@@ -1,13 +1,20 @@
 <?
 include_once("converter.php");
 
-function encode ($array, $key) { //rsa encode array with key (public or private key)
+function encode ($array, $e, $n) { //rsa encode array with public keys e and N; decode must be include separetly 
 	$encoded = array();
 	foreach ($array as $k) {
-		
+		$k = mod($k, $e, $n);
 	}
-		
 	return $encoded;
+}
+
+function decode ($array, $d, $n) { //rsa decode array with privat key d and public key N
+	
+}
+
+function mod ($basis, $exponent, $n) { //returns the result of basis^exponent modulo n
+	
 }
 
 function encrypt ($string, $pubkey) { // rsa encrypt string with pubkey and return database string
