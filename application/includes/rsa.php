0fe35da0e6364314025cce4e2f8e5c35de4bb06d
Enceladus <enceladus.2.89@gmail.com>
2015-05-18 14:20:23 +0200

    changing german into english

--- application/includes/rsa.php
+++ application/includes/rsa.php
@@ -3,16 +3,16 @@ include_once("converter.php");
 
 
 function mod ($basis, $exponent, $mod) { //returns the result of basis^exponent modulo mod
-    $ergebnis = 1;
+    $result = 1;
     while ($exponent > 0) {
      while (bcmod($exponent, 2) == 0) {
       $exponent = $exponent / 2;
       $basis = bcmod($basis * $basis, $mod);
      }
      $exponent = $exponent - 1;
-     $ergebnis = bcmod($ergebnis * $basis, $mod);
+     $result = bcmod($result * $basis, $mod);
     }
-    return $ergebnis;		
+    return $result;		
 }
 
 function encode ($array, $key, $keyN) { //rsa encode array with public keys e and N; decode must be include separetly 
