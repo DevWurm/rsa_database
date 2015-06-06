1c1a46e79fbf243d57fd0e6dd729eb6b6661aa7f
DevWurm <devwurm@gmx.net>
2015-06-06 23:51:58 +0200

    Adapt getSessionState

--- application/includes/verifyKeys.php
+++ application/includes/verifyKeys.php
@@ -1,34 +1,33 @@
 <?php
-    @session_start();
     
     function getSessionState(){
         
+		if (session_status() == PHP_SESSION_DISABLED) {
+			session_start();
+		}
+		
         $pub_keyState = false;
         $priv_keyState = false;
         
-        if(isset($_SESSION['pub_key'])){
-            if($_SESSION['pub_key'] == true){
-                $pub_keyState = true;
-            }
+        if(isset($_SESSION['key_pub'])){
+			$pub_keyState = true;
         }
-        if(isset($_SESSION['priv_key'])){
-            if($_SESSION['priv_key'] == true){
-                $priv_keyState = true;
-            }
+        if(isset($_SESSION['key_priv'])){
+			$priv_keyState = true;
         }
         
-        if($pub_keyState){
-            if($priv_keyState){
-                return 3; //Lese und Schreibrecht
-            }else{
-                return 1; //nur Schreibrecht
-            }
-        }else{
-            if($priv_keyState){
-                return 2; //nur Leserecht
-            }
-        }
-        return 0; //keine Rechte
+		if ($priv_keyState && $pub_keyState) {
+			return 3; //write and read permissions
+		}
+		elseif ($priv_keyState) {
+			return 2; //read permissions
+		}
+		elseif ($pub_keyState) {
+			return 1; //write permissions
+		}
+		else {
+			return 0; //no permissions
+		}
         
     }
 ?>
